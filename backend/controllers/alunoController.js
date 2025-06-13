5affea3ee303699c6f5c01529aeec7b0f0a1776c
chugo@ipvc.pt <hugosantoscarvalho2004@gmail.com>
2025-06-13 10:32:27 +0100

    atualizaçao do alunoConsole

--- backend/controllers/alunoController.js
+++ backend/controllers/alunoController.js
@@ -1,17 +1,18 @@
-const Aluno = require('../models/Aluno');
+const Aluno = require('../models/Aluno'); // ou caminho para o teu modelo
 
-exports.listar = async (req, res) => {
-  const alunos = await Aluno.find();
-  res.json(alunos);
-};
-
-exports.criar = async (req, res) => {
-  const novo = new Aluno(req.body);
-  await novo.save();
-  res.status(201).json(novo);
-};
-
-exports.apagar = async (req, res) => {
-  await Aluno.findByIdAndDelete(req.params.id);
-  res.status(204).end();
+// Atualizar aluno por ID
+exports.atualizarAluno = async (req, res) => {
+  try {
+    const alunoAtualizado = await Aluno.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // devolve o documento atualizado
+    );
+    if (!alunoAtualizado) {
+      return res.status(404).json({ mensagem: 'Aluno não encontrado.' });
+    }
+    res.json(alunoAtualizado);
+  } catch (erro) {
+    res.status(500).json({ mensagem: 'Erro ao atualizar aluno.', erro });
+  }
 };
--- backend/routes/alunoRoutes.js
+++ backend/routes/alunoRoutes.js
@@ -5,5 +5,6 @@ const alunoCtrl = require('../controllers/alunoController');
 router.get('/', alunoCtrl.listar);
 router.post('/', alunoCtrl.criar);
 router.delete('/:id', alunoCtrl.apagar);
+router.put('/alunos/:id', alunoController.atualizarAluno);
 
 module.exports = router;
