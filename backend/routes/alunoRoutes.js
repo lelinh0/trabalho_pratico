d1b6adfafe4c479655c91be318b3e43ac29c6466
chugo@ipvc.pt <hugosantoscarvalho2004@gmail.com>
2025-06-13 10:47:40 +0100

    atualizaçoes pontuais de atualizaçao do aluno

--- backend/routes/alunoRoutes.js
+++ backend/routes/alunoRoutes.js
@@ -5,6 +5,6 @@ const alunoCtrl = require('../controllers/alunoController');
 router.get('/', alunoCtrl.listar);
 router.post('/', alunoCtrl.criar);
 router.delete('/:id', alunoCtrl.apagar);
-router.put('/alunos/:id', alunoController.atualizarAluno);
+router.put('/:id', alunoCtrl.atualizarAluno);
 
 module.exports = router;
